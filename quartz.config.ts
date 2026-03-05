246149d7e6a99b63f715601710e305a335a8ffe0
Sinhwa Hong <budnarae1001@gmail.com>
2026-03-05 17:39:23 +0900

    꼬인 내역 복구

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Budnarae",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -67,6 +67,10 @@ const config: QuartzConfig = {
         keepBackground: false,
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.R2Video({
+        baseUrl: "https://pub-44e440a3a3094ebdb6410c42138d1732.r2.dev",
+        prefix: "videos",
+      }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
--- quartz/plugins/index.ts
+++ quartz/plugins/index.ts
@@ -54,3 +54,5 @@ declare module "vfile" {
     relativePath: FilePath
   }
 }
+
+export { R2Video } from "./transformers/r2Video"
